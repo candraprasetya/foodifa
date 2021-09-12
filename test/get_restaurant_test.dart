56ca3a6e2aae9e974c7e64bdbee5b5a76996c27b
Candra Prasetya <candraramadhanp@gmail.com>
2021-09-12 15:32:42 +0800

    test

--- test/get_restaurant_test.dart
+++ test/get_restaurant_test.dart
@@ -1,16 +1,20 @@
-import 'package:foodifa/utilities/utilities.dart';
+import 'package:dio/dio.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 
 void main() async {
-  final dio = await getApiClient('list');
+  final dio = Dio();
   final dioAdapter = DioAdapter(dio: dio);
 
+  dio.httpClientAdapter = dioAdapter;
+
+  const path = 'https://restaurant-api.dicoding.dev/list';
+
   dioAdapter.onGet(
-    dio.options.baseUrl,
-    (server) => server.reply(200, {'message': 'Success!'}),
+    path,
+    (request) => request.reply(200, {'message': 'Success!'}),
   );
 
-  final response = await dio.get(dio.options.baseUrl);
+  final response = await dio.get(path);
 
   print(response.data); // {message: Success!}
 }
